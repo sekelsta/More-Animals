5566a10df819417c103be4473cfcf0a5f7f8ed7e
sekelsta <sekelsta@protonmail.com>
2025-11-24 06:52:11 -0600

    Remove all remaining code and become a content mod instead

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 # To define the environment variable, put something like this in your .bashrc file:
-# export VINTAGE_STORY_DEV="$HOME/software/vintagestory_dev"
+# export VINTAGE_STORY_DATA="$HOME/.config/VintageStoryData"
+
+# Intention: VINTAGE_STORY points to your Vintage Story install location
+# while VINTAGE_STORY_DATA points to your Vintage Story data location
+# Depending on how you set up those environment variables, you could use
+# a different game install between playing and devving, or just a different
+# data folder location (using the --dataPath argument on startup)
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -11,8 +17,6 @@ if [[ -n $null_textured_shapes ]]; then
     echo -e "${null_textured_shapes}${NC}"
 fi
 
-dotnet run --project ./Build/CakeBuild/CakeBuild.csproj -- "$@"
-rm -r bin/
-rm -r src/obj/
-rm "${VINTAGE_STORY_DEV}"/Mods/MoreAnimals-*.zip
-cp Build/Releases/MoreAnimals-*.zip "${VINTAGE_STORY_DEV}/Mods"
+zip -r MoreAnimals-1.7.0.zip assets/ modicon.png modinfo.json
+rm "${VINTAGE_STORY_DATA}"/Mods/MoreAnimals*.zip
+mv MoreAnimals*.zip "${VINTAGE_STORY_DATA}/Mods"
