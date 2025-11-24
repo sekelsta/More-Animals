a869628defca781c37332a6ffbbd5dd758912fe0
sekelsta <sekelsta@protonmail.com>
2025-11-24 05:34:58 -0600

    Remove logic for updating worlds to new nestbox system, because it was causing duplications in some cases

--- src/MoreAnimals.cs
+++ src/MoreAnimals.cs
@@ -19,32 +19,5 @@ namespace MoreAnimals
     
     public class MoreAnimalsModSystem : ModSystem
     {
-        public override void Start(ICoreAPI api)
-        {
-            api.RegisterBlockEntityClass("BlockNestbox", typeof(BlockEntityNestBox));
-        }
-    }
-
-
-
-    public class BlockEntityNestBox : BlockEntityHenBox
-    {
-        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldForResolving)
-        {
-            base.FromTreeAttributes(tree, worldForResolving);
-            for (int i = 0; i < 10; i++)
-            {
-                string eggCode = tree.GetString("egg" + i);
-                if (eggCode == null) continue;
-                TreeAttribute chick = (TreeAttribute) inventory[i].Itemstack?.Attributes["chick"];
-                CollectibleObject egg = (CollectibleObject)Api.World.GetBlock(eggCode) ?? (CollectibleObject)Api.World.GetItem(eggCode);
-                if (egg == null) {
-                    Api.World.Logger.Warning("Failed to resolve egg code {0} for {1} doing nestbox update", eggCode, Block.Code);
-                    continue;
-                }
-                inventory[i].Itemstack = new ItemStack(egg, 1);
-                inventory[i].Itemstack.Attributes["chick"] = chick;
-            }
-        }
     }
 }
